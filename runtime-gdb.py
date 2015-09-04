ff3e5a54e2028e499520bcc9507a92b71392b5ac
Jason <shengjie5@staff.weibo.com>
2015-09-04 12:13:31 +0800

    remove object support.

--- runtime-gdb.py
+++ runtime-gdb.py
@@ -8,6 +8,7 @@ Author: Jason Young <red.wolf.s.husband@gmail.com>
 from __future__ import print_function
 import re
 import sys 
+import traceback
 
 print("Loading PHP Internal Debug Printer...DONE?", file=sys.stderr)
 
@@ -22,27 +23,31 @@ class PHPZvalPrinter():
     def to_string(self):
         ztype = self.val['u1']['v']['type'];
 
+        if (ztype == 2):
+            return "(FALSE) "
+        if (ztype == 3):
+            return "(TRUE) "
         if (ztype == 4):
-            return "(IS_LONG) " + str(self.val['value']['lval'])
+            return "(LONG) " + str(self.val['value']['lval'])
         
         elif (ztype == 5):
-            return "(IS_DOUBLE) " + str(self.val['value']['dval'])
+            return "(DOUBLE) " + str(self.val['value']['dval'])
 
         elif (ztype == 6): 
             zend_str = self.val['value']['str']
-            return PHPZendStringPrinter(zend_str).to_string();
+            return "(ZVAL) {" + PHPZendStringPrinter(zend_str).to_string() + "}";
         
         elif (ztype == 7): 
-            return PHPHashTablePrinter(self.val['value']['arr']).to_string();
+            return "(ZVAL) {" + PHPHashTablePrinter(self.val['value']['arr']).to_string() + "}";
         
-        elif (ztype == 8):#zend_object 
-            return PHPZendObjectPrinter(self.val['value']['obj']).to_string();
+        #elif (ztype == 8):#zend_object 
+        #    return "(ZVAL) {" + PHPZendObjectPrinter(self.val['value']['obj']).to_string() + "}";
 
-        elif (ztype == 10):#zend_reference
-            zend_reference = self.val['value']['ref']
-            return PHPZendReferencePrinter(zend_reference).to_string();
+        #elif (ztype == 10):#zend_reference
+            #zend_reference = self.val['value']['ref']
+            #return "(ZVAL) {" + PHPZendReferencePrinter(zend_reference).to_string() + "}";
         else:
-            return 'NULL'
+            return 'No pretty printer[type=' + str(ztype) + ']';
 
     def display_hint(self):
         return 'zval *'
@@ -54,10 +59,16 @@ class PHPZendObjectPrinter():
         self.val = val;
     
     def to_string(self):
-        return "(IS_OBJECT) {refcount=" + str(self.val['gc']['refcount']) + ", ce={\n\t" + PHPZendCEPrinter(self.val['ce']).to_string()  + "\n}, props={\n\t" +PHPHashTablePrinter(self.val['properties']).to_string() +"}\n}";
+
+        _str = "";
+        _str += "(OBJECT) {gc=" + str(self.val['gc']['refcount']);
+        _str += ", ce={" + PHPZendCEPrinter(self.val['ce']).to_string();
+        _str += "}, props={\n\t" +PHPHashTablePrinter(self.val['properties']).to_string()
+        _str += "}\n}";
+        return _str;
     
     def display_hit(self):
-        "return zend_reference *"
+        "return zend_object *"
 
 class PHPZendCEPrinter():
     "print zend_class_entry"
@@ -78,7 +89,7 @@ class PHPZendReferencePrinter():
         self.val = val;
     
     def to_string(self):
-        return "(IS_REFERENCE) {refcount=" + str(self.val['gc']['refcount']) + ", val={\n\t" + PHPZvalPrinter(self.val['val']).to_string()  + "\n}}";
+        return "(REFERENCE) {gc=" + str(self.val['gc']['refcount']) + ", val={\n\t" + PHPZvalPrinter(self.val['val']).to_string()  + "\n}}";
     
     def display_hit(self):
         "return zend_reference *"
@@ -93,38 +104,29 @@ class PHPZendStringPrinter():
     
     #Still need to work on this. cause len is not actual string len
     def to_string(self):
-        len = int(self.val['len'])
+        #if str(self.val.address) == '0x0':
+        #    return;
+        size = int(self.val['len'])
 
-        if len > 20:
-            len = 20
-            etc = "..."
-       
         #_str = self.val['val'].string('iso8859-1', 'ignore', len);
         #_str = "len = " + str(len);
-        _str = "" 
+        _str = ""
+        len = 0
+        for i in xrange(size):
+            if (int(self.val['val'][0]) == 0):
+                break;
+            else:
+                len = len+1;
+
         if (int(self.val['val'][0]) == 0):
             _str += "nil";
         else:
             _str = self.val['val'].string('iso8859-1', 'ignore', len);
 
-        return "(IS_STRING) {refcount=" + str(self.val['gc']['refcount']) + ", [" + _str + "...]}"
+        return "(STRING) {gc=" + str(self.val['gc']['refcount']) + ", size=" +str(size)+ ", [" + _str + "]}"
     
     def display_hint(self):
         return 'zend_string *'
-
-
-class PHPZendReferencePrinter():
-    "print zend reference"
-
-    def __init__(self, val):
-        self.val = val;
-    
-    def to_string(self):
-        return "(IS_REFERENCE) {refcount=" + str(self.val['gc']['refcount']) + ", val={\n\t" + PHPZvalPrinter(self.val['val']).to_string()  + "\n}}";
-    
-    def display_hit(self):
-        "return zend_reference *"
-
 
 
 class PHPHashTablePrinter():
@@ -134,6 +136,8 @@ class PHPHashTablePrinter():
         self.val = val;
 
     def to_string(self):
+        if str(self.val) == '0x0':
+            return '0x0';
         nNumOfElements = self.val['nNumOfElements'];
         nTableSize = self.val['nTableSize'];
         nNumUsed = self.val['nNumUsed'];
@@ -142,25 +146,30 @@ class PHPHashTablePrinter():
 
             pNum = 0;
             #only display 4 elements 
-            if (nNumOfElements >= 20):
+            if (nNumUsed >= 20):
                 pNum = 20;
             else:
-                pNum = nNumOfElements;
-            _str += " try to print" + str(pNum)  + "elements";
+                pNum = nNumUsed;
 
             for i in xrange(pNum):
                 try:
-                    key = self.val['arData'][i]['key']
-                    val = self.val['arData'][i]['val']
+                    key = self.val['arData'][i]['key'];
                     _str += PHPZendStringPrinter(key).to_string() + " => "
-                    _str += PHPZvalPrinter(val).to_string() + "\n"
                 except:
+                    _str += "\t" + str(i) + " => "
+                try:
+                    val = self.val['arData'][i]['val'];
+                    _str += PHPZvalPrinter(val).to_string() + "\n"
+                except Exception as i:
                     _str += "unknow";
                     continue;
-        except:
-            return '(HashTable) 0x0?'
+            return "(HT) " + str(self.val[0].address) + " " + _str;
+        except Exception as inst:
+            print (type(inst));
+            traceback.print_exc(file=sys.stdout)
+            #return '(HashT) 0x0'
         
-        return "(HashTable) " +  _str
+        return "(HashTable) 0x0"
     
     def display_hint(self):
         return 'zend_string *'
@@ -171,8 +180,10 @@ def zval_lookup_function (val):
         return PHPZvalPrinter(val)
     if str(val.type) == "zend_string *":
         return PHPZendStringPrinter(val)
-    if str(val.type) == "HashTable *":
-        return PHPHashTablePrinter(val)
+    #if str(val.type) == "zend_object *":
+    #    return PHPZendObjectPrinter(val)
+    #if str(val.type) == "HashTable *":
+    #    return PHPHashTablePrinter(val)
     return None
 
 gdb.pretty_printers.append(zval_lookup_function)
